efd76e8abc4f2d592428549da950430589ab6c6d
Phat Tran <phat@inspectorio.com>
2019-05-24 15:42:40 +0700

    Update Example

--- example/src/app/app.component.ts
+++ example/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'example';
+
+  constructor(
+    private translateService: TranslateService,
+  ) {
+    this.title = translateService.instant('This is the key using by translate service in controller');
+  }
 }
